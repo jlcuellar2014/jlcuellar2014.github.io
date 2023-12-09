f036d0da0fa9a4903c112e42890c7cd0dce4ede8
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-12-09 17:59:52 +0100

    first version of the response disign was made

--- js/site.js
+++ js/site.js
@@ -4,14 +4,27 @@ $(document).ready(function () {
     $.getJSON(url, function (data) {
         showData(data);
     });
+
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#nav-sections'
+      });
 });
 
 function showData(data) {
+
+    var your_name = $(".your-name");
+    var title = $(".your-title");
+    var role = $(".your-role");
+
     var about_me_section = $("#about-me");
     var work_experience = $("#work-experience");
 
-    about_me_section.append(data["about-me"]);
+    your_name.append(data["your-name"]);
+    title.append(data["title"]);
+    role.append(data["role"]);
 
+    about_me_section.append(data["about-me"]);
+    
     data["work-experience"].forEach(e => {
 
         var item = "";
