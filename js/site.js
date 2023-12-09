a4e30978b13f911f3263b9d27bab1ddf618946ef
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-12-09 01:59:47 +0100

    more changes

--- js/site.js
+++ js/site.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     var url = 'data.json';
 
     $.getJSON(url, function (data) {
-        // Función que se ejecuta después de cargar los datos
         showData(data);
     });
 });
@@ -11,60 +10,31 @@ function showData(data) {
     var about_me_section = $("#about-me");
     var work_experience = $("#work-experience");
 
-    console.log(data["work-experience"]);
     about_me_section.append(data["about-me"]);
 
     data["work-experience"].forEach(e => {
 
         var item = "";
-        item += "<li class=\"row\">";
-        item += "   <div class=\"col-4\">"
-        item += "       <p>" + e["company"] + "</p>";
-        item += "       <p>" + e["place"] + "</p>";
-        item += "       <p>" + e["start"] + " - " + e["end"] + "</p>";
-        item += "   </div>"
-        item += "   <div class=\"col\">"
-        item += "       <p>" + e["role"] + "</p>";
-        item += "       <p>" + e["roleDescription"] + "</p>";
-
-        item += "       <ul>";
+        item += "<section>";
+        item += "   <div class=\"row\">";
+        item += "       <div class=\"col-4\">";
+        item += "           <h5>" + e["company"] + "</h5>";
+        item += "           <p>" + e["place"] + "</p>";
+        item += "           <p>" + e["start"] + " - " + e["end"] + "</p>";
+        item += "       </div>";
+        item += "       <div class=\"col\">";
+        item += "           <h5>" + e["role"] + "</h5>";
+        item += "           <p>" + e["roleDescription"] + "</p>";
+        item += "           <ul>";
         e["responsabilities"].forEach(r => {
-            console.log(r);
-            item += "       <li>";
-            item += "           <p>" + r + "</p>";
-            item += "       </li>";
+            item += "           <li>" + r + "</li>";
         });
-        item += "       </ul>";
-
+        item += "           </ul>";
+        item += "       </div>";
         item += "   </div>"
-        item += "</div>"
+        item += "</section>";
 
-        // item += "<li class=\"row\">";
-        // item += "   <div class=\"col\">"
-        // item += "       <p>" + e["company"] + "</p>";
-        // item += "       <p>" + e["role"] + "</p>";
-        // item += "   </div>"
-        // item += "</div>"
-
-        // item += "<li class=\"row\">";
-        // item += "   <div class=\"col\">"
-        // item += "       <p>" + e["place"] + "</p>";
-        // item += "       <p>" + e["roleDescription"] + "</p>";
-        // item += "   </div>"
-        // item += "</div>"
-
-        // item += "<li class=\"row\">";
-        // item += "   <div class=\"col\">"
-        // item += "       <p>" + e["start"] + " - " + e["end"] + "</p>";
-        // item += "       <p>" + e["roleDescription"] + "</p>";
-        // item += "   </div>"
-        // item += "</div>"
-
-
-        item += "</li>";
-
-        console.log(item);
-
+        // console.log(item);
         work_experience.append(item);
     });
 }
